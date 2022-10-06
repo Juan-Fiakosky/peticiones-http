e54d7d9db2b67c7e103af3b22445434984467c04
jfiakosky <jfiakosky@venturihnos.local>
2022-10-06 16:53:21 -0300

    prueba peticiones http

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// fetch( jokeUrl ).then(resp => {
+
+// import { obtenerUsuarios } from "./js/http-provider";
+
+// import { init } from "./js/usuarios-page";
+
+
+// init();
+import * as CRUD from "./js/crud-provider";
+
+
+
+// CRUD.getUsuario( 2 ).then( console.log );
+
+// CRUD.creaarUsuario({
+//     name: "Juan",
+//     job: "Analista"
+// }).then( console.log );
+
+// CRUD.actualizarUsuario( 1, {
+//     name: "Fer",
+//     job: "Diseñadora"
+// }).then( console.log );
+
+CRUD.borrarUsuario( 1 ).then( console.log );
